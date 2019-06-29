f032045159c59f5bab7a4bfcba47e72de84fedc0
goblockme <git@goblockme.de>
2019-06-29 19:29:21 +0200

    Point entry point to new main module

---
+++ polish_case_trainer/polish_case_trainer.py
@@ -0,0 +1,10 @@
+from .root_controller import Controller
+
+
+def main():
+    contr = Controller()
+    contr.main()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages, setup
 
-config = {
-    'description': 'Polish Case Trainer',
-    'author': 'Thomas Busby',
-    'url': 'https://github.com/tombusby/PolishCaseTrainer',
-    'download_url': 'https://github.com/tombusby/PolishCaseTrainer',
-    'author_email': 'tom@busby.ninja',
-    'version': '0.1',
-    'install_requires': ['nose', 'mock'],
-    'packages': ['polish_case_trainer'],
-    'scripts': ['bin/polish_case_trainer'],
-    'data_files': [('polish_case_trainer/word-data', [
+setup(
+    description='Polish Case Trainer',
+    author='Thomas Busby',
+    url='https://github.com/tombusby/PolishCaseTrainer',
+    download_url='https://github.com/tombusby/PolishCaseTrainer',
+    author_email='tom@busby.ninja',
+    version='0.1',
+    install_requires=['inquirer'],
+    packages=find_packages(exclude=['tests']),
+    data_files=[('polish_case_trainer/word-data', [
         'polish_case_trainer/word-data/nouns.json',
         'polish_case_trainer/word-data/adjectives.json'
     ])],
-    'name': 'polish_case_trainer'
-}
-
-setup(**config)
+    name='polish_case_trainer',
+    entry_points={
+        'console_scripts': [
+            'polish_case_trainer=polish_case_trainer.polish_case_trainer:main',
+        ],
+    },
+)
